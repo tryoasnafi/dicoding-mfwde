fcf3a951664a9766bece43c9359d352fddf644f5
Tryo Asnafi <tryoasnafi@gmail.com>
2022-04-14 07:11:01 +0700

    update: wrapping card content

--- src/scripts/components/food-item.js
+++ src/scripts/components/food-item.js
@@ -11,8 +11,10 @@ class FoodItem extends HTMLElement {
   render() {
     this.innerHTML = `
       <a href="#">
-        <img src="${this._food.image}" alt="${this._food.title}" class="food-image" loading="lazy" />
-        <h4 class="card__title text-overflow">${this._food.title}</h4>
+        <section>
+          <img src="${this._food.image}" alt="${this._food.title}" class="food-image" loading="lazy" />
+          <h4 class="card__title text-overflow">${this._food.title}</h4>
+        </section>
       </a>
     `
   }
