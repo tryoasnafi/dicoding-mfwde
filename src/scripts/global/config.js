57f250e6cf82d738d9ad73736bcf6ca568de84ad
Tryo Asnafi <tryoasnafi@gmail.com>
2022-04-21 22:13:58 +0700

    update config

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,10 +1,9 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev',
-  CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: 'resfi-cache',
   DATABASE_NAME: 'restaurant-db',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurants',
-  WEB_SOCKET_SERVER: 'wss://movies-feed.dicoding.dev',
 };
 
 export default CONFIG;
