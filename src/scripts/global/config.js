9d4a69009e53586b97194797d58608cc399c58d7
Tryo Asnafi <tryoasnafi@gmail.com>
2022-04-21 23:20:21 +0700

    fix: skip to content

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev',
-  CACHE_NAME: 'resfi-cache',
+  CACHE_NAME: +new Date(),
   DATABASE_NAME: 'restaurant-db',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurants',
