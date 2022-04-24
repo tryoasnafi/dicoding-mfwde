b07353b29eae94d329c2aba18a8166e8af0c595b
Tryo Asnafi <tryoasnafi@gmail.com>
2022-04-24 13:36:59 +0700

    rename constanta base_url to api_url

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,6 +1,7 @@
 const CONFIG = {
-  BASE_URL: 'https://restaurant-api.dicoding.dev',
+  API_URL: 'https://restaurant-api.dicoding.dev',
   CACHE_NAME: 'resfi-cache',
+  CACHE_VERSION: 1,
   DATABASE_NAME: 'restaurant-db',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurants',
