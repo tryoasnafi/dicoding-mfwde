35106c440f082fd832a987a50741aac9d9899942
Tryo Asnafi <tryoasnafi@gmail.com>
2022-05-09 15:34:45 +0700

    fix: if data not has id

--- src/scripts/utils/favorite-restaurant-idb.js
+++ src/scripts/utils/favorite-restaurant-idb.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-prototype-builtins */
+/* eslint-disable consistent-return */
 import { openDB } from 'idb';
 import CONFIG from '../global/config';
 
@@ -11,6 +13,7 @@ const dbPromise = openDB(DATABASE_NAME, DATABASE_VERSION, {
 
 const FavoriteRestaurantIdb = {
   async getRestaurant(id) {
+    if (!id) return;
     return (await dbPromise).get(OBJECT_STORE_NAME, id);
   },
 
@@ -18,8 +21,11 @@ const FavoriteRestaurantIdb = {
     return (await dbPromise).getAll(OBJECT_STORE_NAME);
   },
 
-  async putRestaurant(movie) {
-    return (await dbPromise).put(OBJECT_STORE_NAME, movie);
+  async putRestaurant(restaurant) {
+    if (!restaurant.hasOwnProperty('id')) {
+      return;
+    }
+    return (await dbPromise).put(OBJECT_STORE_NAME, restaurant);
   },
 
   async deleteRestaurant(id) {
